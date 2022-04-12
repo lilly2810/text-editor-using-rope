2146f1cddc8719103456afb773b38f41f302c0dc
lilly2810 <85385602+lilly2810@users.noreply.github.com>
2022-04-13 01:28:33 +0530

    Add files via upload

--- ropett.cpp
+++ ropett.cpp
@@ -19,8 +19,7 @@ text_t * create_text() {
 	tmp_node->key = 0;
 	tmp_node->height = 0;
 	insert_line(tmp_node,1,"ADVANCED DATA STRUCTURES");
-	return tmp_node;
-}
+	return tmp_node;}
 
 void left_rotation(text_t *n) {
 	text_t *tmp_node;
@@ -29,8 +28,7 @@ void left_rotation(text_t *n) {
 	n->right = n->left->right;
 	n->left->right = n->left->left;
 	n->left->left = tmp_node;
-	n->left->key = n->left->left->key + n->left->right->key;
-}
+	n->left->key = n->left->left->key + n->left->right->key;}
 
 void right_rotation(text_t *n) {
 	text_t *tmp_node;
@@ -39,8 +37,7 @@ void right_rotation(text_t *n) {
 	n->left = n->right->left;
 	n->right->left = n->right->right;
 	n->right->right = tmp_node;
-	n->right->key = n->right->left->key + n->right->right->key;
-}
+	n->right->key = n->right->left->key + n->right->right->key;}
 
 int length_text(text_t *txt) {
 	text_t *temp = txt;
@@ -49,9 +46,7 @@ int length_text(text_t *txt) {
 	} else if (temp->left == NULL) {
 		return 0;
 	} else {
-		return txt->key;
-	}
-}
+		return txt->key;}}
 
 char * get_line(text_t *txt, int index) {
 	text_t *tmp_node = txt;
@@ -67,17 +62,12 @@ char * get_line(text_t *txt, int index) {
 			if (index <= tmp_node->left->key) {
 				//Go left
 				tmp_node = tmp_node->left;
-
 			} else {
 				//Update index and Go right
 				index = index - tmp_node->left->key;
-				tmp_node = tmp_node->right;
-			}
-		}
+				tmp_node = tmp_node->right;}}
 		//Node found; return line
-		return (char *) tmp_node->left;
-	}
-}
+		return (char *) tmp_node->left;}}
 
 char * set_line(text_t *txt, int index, char * new_line) {
 	text_t *tmp_node = txt;
@@ -93,23 +83,14 @@ char * set_line(text_t *txt, int index, char * new_line) {
 			if (index <= tmp_node->left->key) {
 				//Go left
 				tmp_node = tmp_node->left;
-
 			} else {
 				//Update index and Go right
 				index = index - tmp_node->left->key;
-				tmp_node = tmp_node->right;
-			}
-		}
-		//Node found; replace and return
+				tmp_node = tmp_node->right;}}
 		char *old_line;
 		old_line = (char *) tmp_node->left;
 		tmp_node->left = (text_t *) new_line;
-
-		return old_line;
-	}
-}
-
-
+		return old_line;}}
 
 void insert_line(text_t *txt, int index, char * new_line) {
 	text_t *tmp_node = txt;
@@ -128,15 +109,12 @@ void insert_line(text_t *txt, int index, char * new_line) {
 				tmp_node->key++;
 				path_stack[path_st_p++] = tmp_node;
 				tmp_node = tmp_node->left;
-
 			} else {
 				//Update index and Go right
 				index = index - tmp_node->left->key;
 				tmp_node->key++;
 				path_stack[path_st_p++] = tmp_node;
-				tmp_node = tmp_node->right;
-			}
-		}
+				tmp_node = tmp_node->right;}}
 
 		text_t *old_leaf, *new_leaf;
 		old_leaf = create_text();
@@ -158,8 +136,7 @@ void insert_line(text_t *txt, int index, char * new_line) {
 			tmp_node = path_stack[--path_st_p];
 			old_height = tmp_node->height;
 			if (tmp_node->left->height - tmp_node->right->height == 2) {
-				if (tmp_node->left->left->height - tmp_node->right->height
-						== 1) {
+				if (tmp_node->left->left->height - tmp_node->right->height== 1) {
 					right_rotation(tmp_node);
 					tmp_node->right->height = tmp_node->right->left->height + 1;
 					tmp_node->height = tmp_node->right->height + 1;
@@ -169,11 +146,9 @@ void insert_line(text_t *txt, int index, char * new_line) {
 					tmp_height = tmp_node->left->left->height;
 					tmp_node->left->height = tmp_height + 1;
 					tmp_node->right->height = tmp_height + 1;
-					tmp_node->height = tmp_height + 2;
-				}
+					tmp_node->height = tmp_height + 2;}
 			} else if (tmp_node->left->height - tmp_node->right->height == -2) {
-				if (tmp_node->right->right->height - tmp_node->left->height
-						== 1) {
+				if (tmp_node->right->right->height - tmp_node->left->height== 1) {
 					left_rotation(tmp_node);
 					tmp_node->left->height = tmp_node->left->right->height + 1;
 					tmp_node->height = tmp_node->left->height + 1;
@@ -183,20 +158,15 @@ void insert_line(text_t *txt, int index, char * new_line) {
 					tmp_height = tmp_node->right->right->height;
 					tmp_node->left->height = tmp_height + 1;
 					tmp_node->right->height = tmp_height + 1;
-					tmp_node->height = tmp_height + 2;
-				}
+					tmp_node->height = tmp_height + 2;}
 			} else {
 				/* update height even if there was no rotation */
 				if (tmp_node->left->height > tmp_node->right->height)
 					tmp_node->height = tmp_node->left->height + 1;
 				else
-					tmp_node->height = tmp_node->right->height + 1;
-			}
+					tmp_node->height = tmp_node->right->height + 1;}
 			if (tmp_node->height == old_height)
-				finished = 1;
-		}
-	}
-}
+				finished = 1;}}}
 
 void append_line(text_t *txt, char * new_line) {
 	text_t *tmp_node;
@@ -213,8 +183,8 @@ void append_line(text_t *txt, char * new_line) {
 		while (tmp_node->right != NULL) {
 			tmp_node->key++;
 			path_stack[path_st_p++] = tmp_node;
-			tmp_node = tmp_node->right;
-		}
+			tmp_node = tmp_node->right;}
+
 		text_t *old_leaf, *new_leaf;
 		old_leaf = create_text();
 		old_leaf->left = tmp_node->left;
@@ -235,10 +205,9 @@ void append_line(text_t *txt, char * new_line) {
 			tmp_node = path_stack[--path_st_p];
 			old_height = tmp_node->height;
 			if (tmp_node->left->height - tmp_node->right->height == 2) {
-				if (tmp_node->left->left->height - tmp_node->right->height
-						== 1) {
+				if (tmp_node->left->left->height - tmp_node->right->height== 1) {
 					if (debug)
-						printf("Inside append 3.1.1\n");
+						printf("Inside append\n");
 					right_rotation(tmp_node);
 					tmp_node->right->height = tmp_node->right->left->height + 1;
 					tmp_node->height = tmp_node->right->height + 1;
@@ -248,11 +217,9 @@ void append_line(text_t *txt, char * new_line) {
 					tmp_height = tmp_node->left->left->height;
 					tmp_node->left->height = tmp_height + 1;
 					tmp_node->right->height = tmp_height + 1;
-					tmp_node->height = tmp_height + 2;
-				}
+					tmp_node->height = tmp_height + 2;}
 			} else if (tmp_node->left->height - tmp_node->right->height == -2) {
-				if (tmp_node->right->right->height - tmp_node->left->height
-						== 1) {
+				if (tmp_node->right->right->height - tmp_node->left->height== 1) {
 					left_rotation(tmp_node);
 					tmp_node->left->height = tmp_node->left->right->height + 1;
 					tmp_node->height = tmp_node->left->height + 1;
@@ -262,20 +229,15 @@ void append_line(text_t *txt, char * new_line) {
 					tmp_height = tmp_node->right->right->height;
 					tmp_node->left->height = tmp_height + 1;
 					tmp_node->right->height = tmp_height + 1;
-					tmp_node->height = tmp_height + 2;
-				}
+					tmp_node->height = tmp_height + 2;}
 			} else {
 				/* update height even if there was no rotation */
 				if (tmp_node->left->height > tmp_node->right->height)
 					tmp_node->height = tmp_node->left->height + 1;
 				else
-					tmp_node->height = tmp_node->right->height + 1;
-			}
+					tmp_node->height = tmp_node->right->height + 1;}
 			if (tmp_node->height == old_height)
-				finished = 1;
-		}
-	}
-}
+				finished = 1;}}}
 
 char * delete_line(text_t *txt, int index) {
 	if (debug) printf("Inside delete\n");
@@ -304,9 +266,7 @@ char * delete_line(text_t *txt, int index) {
 			} else {
 				index = index - upper_node->left->key;
 				tmp_node = upper_node->right;
-				other_node = upper_node->left;
-			}
-		}
+				other_node = upper_node->left;}}
 		upper_node->key = other_node->key;
 		upper_node->left = other_node->left;
 		upper_node->right = other_node->right;
@@ -352,15 +312,18 @@ char * delete_line(text_t *txt, int index) {
 				if (tmp_node->left->height > tmp_node->right->height)
 					tmp_node->height = tmp_node->left->height + 1;
 				else
-					tmp_node->height = tmp_node->right->height + 1;
-			}
+					tmp_node->height = tmp_node->right->height + 1;}
 			if (tmp_node->height == old_height)
-				finished = 1;
-		}
-		return deleted_object;
-	}
-}
-int main(){      
+				finished = 1;}
+		return deleted_object;}}
+
+void print(text_t* root){
+    text_t* temp=root;
+    for(int i=1;i<length_text(temp);i++){
+        cout<<i<<"."<<get_line(root,i)<<endl;}}
+
+int main(){    
+	system("cls");  
     cout<<"\t\t\tTEXT EDITOR USING ROPE ";
 	int i, tmp; text_t *root,*temp; char c[100];
     int ind ,choice,choice1;
@@ -369,57 +332,46 @@ int main(){
 	  insert_line(root,2,"PROJECT DEMO");
 	  insert_line(root,3,"TEXT EDITOR");
 	  insert_line(root,4,"USING ROPE");
-
+	  print(root);
     do{
         cout<<"\n\t\t\t\tMENU";
-        cout<<"\n\t\t\t1. FIND STRING BY INDEX  \n\t\t\t2. FIND AND REPLACE \n\t\t\t3. DELETE \n\t\t\t4. PRINT \n\nEnter your choice: ";
+        cout<<"\n\t\t\t1.INSERT \n\t\t\t2.DELETE \n\t\t\t3.FIND\n\t\t\t4.FIND AND REPLACE \n\nEnter your choice: ";
         cin>>choice;
         switch( choice ){
         case 1:
-                    cout<<"\n ENTER THE INDEX TO FIND THE STRING:";
-                    cin>>ind;
-                    cout<<get_line(root,ind);
-                    break;
-
+				   cout<<"\nENTER THE INDEX";
+				   cin>>ind;
+                   cout<<"\n ENTER THE TEXT\n";
+                   cin>>c;
+                   insert_line(root,ind,c);
+                   cout<<endl;break; 
         case 2:
+		            cout<<"\n ENTER THE INDEX:";
+                    cin>>ind;
+                    if(delete_line( root,ind)== NULL){
+                        cout<<"THE INDEX: "<<ind<<" NOT IN THE FILE";}
+                    cout<<endl;
+                    break;
+        case 3:
+		            cout<<"\n ENTER THE INDEX:";
+                    cin>>ind;
+                    cout<<get_line(root,ind)<<endl;
+                    break;
+        case 4:    
                     temp=root;
                     cout<<"\nREPLACE WITH : ";
                     cin>>c;
                     cout<<"\n ENTER THE INDEX TO FIND THE STRING:";
                     cin>>ind;
                     if( get_line(temp,ind)!=NULL )
-                        cout<<set_line(temp,ind,c);
+                        set_line(temp,ind,c);
                     else
-                        cout<<"\nNot in file..!!\n";
+                        cout<<"\nNot in Text\n";
                     break;
-
-        case 3:
-                    
-                    cout<<"\n ENTER THE INDEX TO FIND THE STRING:";
-                    cin>>ind;
-                    if(delete_line( root,ind)== NULL){
-                        cout<<"THE INDEX: "<<ind<<" NOT IN THE FILE";}
-                    cout<<endl;
-                    break;
-
-        case 4:
-                
-           temp=root;
-                    for(int i=1;i<length_text(temp);i++){
-                        {cout<<i<<"."<<get_line(root,i)<<endl;}}
-                    break;
-        case 5:    
-                   cout<<"insert";
-				   cout<<"enter line no";
-				   cin>>ind;
-                   cout<<"\n ENTER THE PARAGRAPH\n";
-                   cin>>c;
-                   insert_line(root,ind,c);
-                   cout<<endl;break;
 
         default:
                     cout<<"\nINVALID OPTION";}
+					print(root);
         cout<<"\n \n 1 - CONTINUE \n 0 - EXIT";
         cin>>choice1;
-    }while( choice1 );
-return 0;}
+    }while( choice1 );}
